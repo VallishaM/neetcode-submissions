684fdf4f90856437aca2b8a7d66d85a48645d3f7
VallishaM <71862949+VallishaM@users.noreply.github.com>
2026-06-18 07:09:39 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freqs = []
+        mem={}
+        ord_a = ord('a')
+        for s in strs:
+            fn=[0]*26
+            for c in s:
+                fn[ord(c) - ord_a] += 1
+            if tuple(fn) in mem:
+                mem[tuple(fn)].append(s)
+            else:
+                mem[tuple(fn)] = [s]
+        return list(mem.values())
