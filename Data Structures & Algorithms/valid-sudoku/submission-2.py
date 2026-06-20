e9e9c735a681288a4dc2c7f27cb989d45b548748
VallishaM <71862949+VallishaM@users.noreply.github.com>
2026-06-20 13:44:04 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        square_mem=[set() for _ in range(9)]
+        #Checking rows for duplicates
+        for i in range(9):
+            row_mem = set()
+            column_mem = set()
+            
+            for j in range(9):
+                if board[i][j] in row_mem:
+                    return False
+                if board[i][j] !='.':
+                    row_mem.add(board[i][j])
+                if board[j][i] in column_mem:
+                    return False
+                if board[j][i] !='.':
+                    column_mem.add(board[j][i])
+                
+                square_number = int(i/3) * 3 + int(j/3)
+                
+                if board[i][j] in square_mem[square_number]:
+                    return False
+                if board[i][j] != '.':
+                    square_mem[square_number].add(board[i][j])
+        
+        return True
